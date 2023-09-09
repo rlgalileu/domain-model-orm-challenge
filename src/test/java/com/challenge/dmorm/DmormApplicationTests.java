3b80e44dde0cf81a3fe842e1e8a3344403cc7888
Galileu Rodrigues de Lima <54282757+rlgalileu@users.noreply.github.com>
2023-09-09 11:55:08 -0300

    Initial commit

---
+++ src/test/java/com/challenge/dmorm/DmormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.dmorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
